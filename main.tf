0b8982bf9abe07d9cf54cbd95bdeeedd35869c8a
Malvika-Grover <malvikagrover22@gmail.com>
2024-03-09 22:57:15 +0530

    VPC part done

--- main.tf
+++ main.tf
@@ -4,16 +4,16 @@ resource "aws_vpc" "VPC_1" {
 }
 
 //Subnet Creation
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet_1" {
   vpc_id = aws_vpc.VPC_1.id
-  cidr_block = var.cidr_public_subnet
+  cidr_block = var.cidr_public_subnet_1
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "public_subnet_2" {
   vpc_id = aws_vpc.VPC_1.id
-  cidr_block = var.cidr_private_subnet
+  cidr_block = var.cidr_public_subnet_2
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 }
@@ -24,3 +24,24 @@ resource "aws_internet_gateway" "IGW" {
   
 }
 
+//Route Table
+resource "aws_route_table" "RT1" {
+  vpc_id = aws_vpc.VPC_1.id
+
+//Defining routes
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+}
+
+//Subnet Association with Route table so that it has internet access
+resource "aws_route_table_association" "rta1" {
+    subnet_id = aws_subnet.public_subnet_1.id
+    route_table_id = aws_route_table.RT1.id 
+}
+
+resource "aws_route_table_association" "rta2" {
+    subnet_id = aws_subnet.public_subnet_2.id
+    route_table_id = aws_route_table.RT1.id 
+}
--- variables.tf
+++ variables.tf
@@ -4,13 +4,13 @@ variable "cidr_vpc" {
   default = "10.0.0.0/16"
 }
 
-variable "cidr_public_subnet" {
+variable "cidr_public_subnet_1" {
   description = "CIDR block for Public Subnet"
   default = "10.0.0.0/24"
   
 }
 
-variable "cidr_private_subnet" {
+variable "cidr_public_subnet_2" {
   description = "CIDR block for Private Subnet"
   default = "10.0.1.0/24"
   
