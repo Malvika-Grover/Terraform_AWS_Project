e3775eb91580b3b1108545fb6976f683567ff551
Malvika-Grover <malvikagrover22@gmail.com>
2024-03-09 22:43:55 +0530

    VPC, SUbnet1 and 2 created successfully

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+//VPC Creation
 resource "aws_vpc" "VPC_1" {
-  cidr_block = var.cidr
+  cidr_block = var.cidr_vpc
+}
+
+//Subnet Creation
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.VPC_1.id
+  cidr_block = var.cidr_public_subnet
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.VPC_1.id
+  cidr_block = var.cidr_private_subnet
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,17 @@
-variable "cidr" {
+
+variable "cidr_vpc" {
   description = "CIDR block for VPC"
   default = "10.0.0.0/16"
+}
+
+variable "cidr_public_subnet" {
+  description = "CIDR block for Public Subnet"
+  default = "10.0.0.0/24"
+  
+}
+
+variable "cidr_private_subnet" {
+  description = "CIDR block for Private Subnet"
+  default = "10.0.1.0/24"
+  
 }
